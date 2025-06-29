6616b92e4b83d39d25781e1f8dbb907ee318b42f
Ken Española <revas4192@gmail.com>
2025-06-29 20:55:39 +0800

    initial commit

--- qrcode/app/(tabs)/index.tsx
+++ qrcode/app/(tabs)/index.tsx
@@ -1,31 +1,22 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'; 
+import React from 'react';
+import { auth } from '@/FirebaseConfig';
+import { getAuth } from 'firebase/auth';
+import { router } from 'expo-router';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+export default function index() {
+  getAuth().onAuthStateChanged((user) => {
+    if (!user) router.replace('/');
 
-export default function TabOneScreen() {
+  });
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <View>
+      <Text>Sign Out </Text>
+      <TouchableOpacity onPress={() => auth.signOut()}>
+        <Text>Sign Out</Text>
+      </TouchableOpacity>
+    </View> 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+const styles = StyleSheet.create({});
